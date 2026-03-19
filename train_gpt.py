b1d51f95fb4e2da39dcedcc725afb0978b3dad0c
joeynyc <joeyr1983@gmail.com>
2026-03-19 08:42:38 -0400

    Revert "Experiment 51: model_dim 512->448 (faster training)"
    This reverts commit 0487a1b853984305a8e1bcf3ab3bb6f27b84a9f7.

--- train_gpt.py
+++ train_gpt.py
@@ -63,7 +63,7 @@ class Hyperparameters:
     vocab_size = int(os.environ.get("VOCAB_SIZE", 1024))
     num_layers = int(os.environ.get("NUM_LAYERS", 9))
     num_kv_heads = int(os.environ.get("NUM_KV_HEADS", 2))
-    model_dim = int(os.environ.get("MODEL_DIM", 448))
+    model_dim = int(os.environ.get("MODEL_DIM", 512))
     num_heads = int(os.environ.get("NUM_HEADS", 4))
     mlp_mult = int(os.environ.get("MLP_MULT", 2))
     tie_embeddings = bool(int(os.environ.get("TIE_EMBEDDINGS", "0")))
