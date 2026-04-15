f7faa2e4977786b5122d2461b77db1ca9437f503
Aditya Sasidhar <telikicherlaadityasasidhar@gmail.com>
2026-04-15 16:23:12 +0530

    qk = 4.5 m_wd = 0.04
    Signed-off-by: Aditya Sasidhar <telikicherlaadityasasidhar@gmail.com>

--- records/track_non_record_16mb/2026-04-14_Single_H100_8L_int6attn_int8mlp_3mlp_GQA_QAT015_1.21/train_gpt.py
+++ records/track_non_record_16mb/2026-04-14_Single_H100_8L_int6attn_int8mlp_3mlp_GQA_QAT015_1.21/train_gpt.py
@@ -69,7 +69,7 @@ class Hyperparameters:
     beta1 = float(os.environ.get("BETA1", 0.9))
     beta2 = float(os.environ.get("BETA2", 0.95))
     adam_eps = float(os.environ.get("ADAM_EPS", 1e-8))
-    muon_wd = float(os.environ.get("MUON_WD", 0.02))
+    muon_wd = float(os.environ.get("MUON_WD", 0.04))
     adam_wd = float(os.environ.get("ADAM_WD", 0.04))
     grad_clip_norm = float(os.environ.get("GRAD_CLIP_NORM", 0.0))
     xsa_last_n = int(os.environ.get("XSA_LAST_N", 2))
