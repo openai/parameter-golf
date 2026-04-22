1d089927381702c102c9e2e3125b1af1f0c076e5
yangguohao <1901212980@pku.edu.cn>
2026-04-21 20:38:05 -0500

    RandomLinearMaps

--- run.sh
+++
@@ -1,6 +0,0 @@
-pip install numpy huggingface-hub datasets tqdm
-pip install brotli sentencepiece
-pip install flash_attn_3 --no-deps --find-links https://windreamer.github.io/flash-attention3-wheels/cu128_torch291/
-MATCHED_FINEWEB_REPO_ID=kevclark/parameter-golf python3 data/cached_challenge_fineweb.py --variant sp8192
-
-
--- train.sh
+++
@@ -1,1 +0,0 @@
-ITERATIONS=2000 TRAIN_LOG_EVERY=20 WARMUP_STEPS=0 MAX_WALLCLOCK_SECONDS=0 torchrun --standalone --nproc_per_node=4 train_baseline.py
