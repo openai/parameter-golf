8ec93653ead6949eabc21cbca43053d6176b9197
Aditya Sasidhar <telikicherlaadityasasidhar@gmail.com>
2026-04-24 12:30:46 +0530

    all xsa layers

--- records/track_non_record_16mb/2026-04-14_Single_H100_8L_int6attn_int8mlp_3mlp_GQA_QAT015_1.21/train_gpt.py
+++ records/track_non_record_16mb/2026-04-14_Single_H100_8L_int6attn_int8mlp_3mlp_GQA_QAT015_1.21/train_gpt.py
@@ -74,7 +74,7 @@ class Hyperparameters:
     muon_wd = float(os.environ.get("MUON_WD", 0.02))
     adam_wd = float(os.environ.get("ADAM_WD", 0.04))
     grad_clip_norm = float(os.environ.get("GRAD_CLIP_NORM", 0.0))
-    xsa_last_n = int(os.environ.get("XSA_LAST_N", 5))
+    xsa_last_n = int(os.environ.get("XSA_LAST_N", 8))
     warmdown_last_frac = float(os.environ.get("WARMDOWN_LAST_FRAC", 0.2))
     use_flash_attn_interface = bool(
         int(os.environ.get("USE_FLASH_ATTN_INTERFACE", "1"))
