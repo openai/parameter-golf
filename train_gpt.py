22d8e519a40352c78b55bdfa2ac736fa61e8f74c
Alston Tang <145715945+AlstonTang@users.noreply.github.com>
2026-05-01 08:56:38 -0700

    Compilation flags

--- train_gpt.py
+++ train_gpt.py
@@ -875,7 +875,7 @@ def main() -> None:
 
     code = Path(__file__).read_text(encoding="utf-8")
     args = Hyperparameters()
-    zeropower_via_newtonschulz5 = torch.compile(zeropower_via_newtonschulz5)
+    zeropower_via_newtonschulz5 = torch.compile(zeropower_via_newtonschulz5, fullgraph=True, mode="reduce-overhead")
 
     # -----------------------------
     # DISTRIBUTED + CUDA SETUP
@@ -983,7 +983,7 @@ def main() -> None:
         if isinstance(module, CastedLinear):
             module.float()
     restore_low_dim_params_to_fp32(base_model)
-    compiled_model = torch.compile(base_model, dynamic=False, fullgraph=True)
+    compiled_model = torch.compile(base_model, dynamic=False, fullgraph=True, mode="reduce-overhead")
     model: nn.Module = DDP(compiled_model, device_ids=[local_rank], broadcast_buffers=False) if distributed else compiled_model
 
     # Optimizer split:
